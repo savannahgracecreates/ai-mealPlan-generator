a37044389c95578be337c9657a3643f90965ee45
Savannah Grace <151082915+savannahgracecreates@users.noreply.github.com>
2024-10-30 20:24:23 -0400

    Added basic form handling and typewriter js plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert ("hello world");
+function generateMealPlan(event){
+    event.preventDefault();
+    new Typewriter("#meal-plan", {
+        strings: "Eat more protein",
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+    }
+
+
+let mealPlanFormElement = document.querySelector("#meal-plan-form");
+mealPlanFormElement.addEventListener("submit", generateMealPlan);
