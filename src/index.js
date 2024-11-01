ca360d97b3b8f7974aa16b3150448021fa375cbd
Savannah Grace <151082915+savannahgracecreates@users.noreply.github.com>
2024-11-01 16:28:47 -0400

    improve the UI
    added CSS classes to help users understand when content is loading and improved formatting.

--- src/index.js
+++ src/index.js
@@ -14,11 +14,13 @@ function generateMealPlan(event){
     let instructionsInput = document.querySelector("#user-instructions");
 let apiKey = "d39d6443520afb97te34a49a61faf9o3";
 let prompt = `Generate a meal plan. Please give clients daily meal plan ideas and utilize ${instructionsInput.value} as a parameter for the meals. You only need to provide ideas for one day.`;
-let context = "You are nutrition coach that helps performance athletes lose weight and maintain muscle by providing a healthy meal plan for the day for people to follow. Your mission is to provide meal ideas for the day in basic HTML and seperate each meal with a <br/>.";
+let context = "You are nutrition coach that helps performance athletes lose weight and maintain muscle by providing a healthy meal plan for the day for people to follow. Your mission is to provide meal ideas for the day in basic HTML and seperate each meal with a <br/>. Do not mention html or HTML in your response, please.";
 let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-console.log("generating a response");
-console.log(`Prompt: ${prompt}`);
-console.log(`Context: ${context}`);
+
+let mealPlanElement = document.querySelector("#meal-plan");
+mealPlanElement.classList.remove("meal-plan-hidden");
+mealPlanElement.innerHTML = `<div class="blink_me"> Generating today's plan with ${instructionsInput.value} in mind.</div>`;
+
 axios.get(apiURL).then(displayMealPlan);
 
 
